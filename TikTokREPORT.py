6cab6bb7712559cabf54380e4eb7ab54a4cc32d7
botzanee <botzane52@gmail.com>
2024-08-17 19:10:41 +0800

    Add files via upload

---
+++ TikTokREPORT.py
@@ -0,0 +1,134 @@
+import requests
+from requests.sessions import session
+import json
+import time
+import colorama
+from colorama import Fore, Back, Style
+import requests
+import time as mm
+import sys as n
+from colorama import *
+import string
+
+req = requests.session()
+
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 5)
+
+colorama.init()
+
+session = requests.session()
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}===========================================================")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 6)
+      
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}██╗  ██╗███████╗███╗   ██╗")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}╚██╗██╔╝██╔════╝████╗  ██║ ")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}╚███╔╝ █████╗  ██╔██╗ ██║")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX} ██╔██╗ ██╔══╝  ██║╚██╗██║ ")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}██╔╝ ██╗███████╗██║ ╚████║")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝  ")
+wacn(f"{Style.BRIGHT}{Fore.RED}POGI SI NIX")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 5)
+      
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}===========================================================")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 2.5)
+      
+wacn(f"{Style.BRIGHT}{Fore.WHITE}THIS IS PRIVATE SRIPT BY NIX ZANE ==[FB: Zen Rei]==")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+wacn(f"{Style.BRIGHT}{Fore.WHITE}A Report Is Send Every 10 Seconds!!!")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}===========================================================")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 2)
+      
+wacn(f"{Style.BRIGHT}{Fore.WHITE}NOTE: THIS SCRIPT WILL NOT AFFECT YOUR SYSTEM, IM NOT RESPONSIBLE IF SOMETHING HAPPENS")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 2.5)
+      
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}===========================================================")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+wacn(f"{Style.BRIGHT}{Fore.WHITE}Welcome To The")
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 7)
+      
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}  _____                       _   ____        _   ")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX} |  __ \                     | | |  _ \      | |  ")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX} | |__) |___ _ __   ___  _ __| |_| |_) | ___ | |_")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX} |  _  // _ \ '_ \ / _ \| '__| __|  _ < / _ \| __|")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX} | | \ \  __/ |_) | (_) | |  | |_| |_) | (_) | |_ ")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX} |_|  \_\___| .__/ \___/|_|   \__|____/ \___/ \__|")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}            | |                                   ")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}            |_|                                   ")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 1)
+      
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}Use This Tool And Have Fun!")
+wacn(f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}")
+
+def wacn(s):
+    for c in s + "\n":
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(0.10/ 2.5)
+      
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}===========================================================")
+wacn(f"{Style.BRIGHT}{Fore.WHITE}")
+
+x = input ('Enter the request URL from Inspect Element: ')
+print("")
+print("")
+
+wacn(f"{Style.BRIGHT}{Fore.MAGENTA}Reporting...")
+print('')
+print('')
+
+while True:
+    req = session.post(x)
+    
+    print(req.text)
+    wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+    wacn(f"{Style.BRIGHT}{Fore.MAGENTA}")
+    wacn(f"{Style.BRIGHT}{Fore.MAGENTA}REPORTED NA KUPAL")
+    time.sleep(10)
+
+input()
